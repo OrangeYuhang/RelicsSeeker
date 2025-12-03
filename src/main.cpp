07bd085e5541332c81dc4605e1e559d6607a6a1c
Yuhang Chen <JoasfainKeny@outlook.com>
2025-12-03 19:44:54 +0800

    添加项目文件。

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "../include/item.h"
+#include "../include/backpack.h"
+#include "../include/event.h"
+#include "../include/player.h"
+#include "../include/map.h"
+#include "../include/shop.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <graphics.h>
+int main() {
+	return 0;
+}
